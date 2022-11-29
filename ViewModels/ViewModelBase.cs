81f9ff299ff609515b4b5b629bb70109e0394fd6
NothingPersonal <destra2017@mail.ru>
2022-11-29 16:37:56 +0700

    Added ViewModelBase and MainWindowViewModel. Added INotifyPropertyChanged and IDisposable interfaces.

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Linq.Expressions;
+
+namespace HEADMEN_EYE.ViewModels
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged, IDisposable
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        //  Сборщик мусора
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private bool _Disposed;
+        protected virtual void Dispose(bool Disposing)
+        {
+            if (!Disposing || _Disposed) return;
+            _Disposed = true;
+            // Освобождение управляемых ресурсов
+        }
+    }
+}
