6b38c5f9cbef0cfb486e8e6ad7260f0b28e5064b
NothingPersonal <destra2017@mail.ru>
2022-12-20 13:43:39 +0700

    Project moved to .NET. Added connection with database.

---
+++ Data/ConnectDB.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HEADMEN_EYE.Data
+{
+    class ConnectDB : DbContext
+    {
+        //  This class connect program with DB
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = HEADMEN_EYE_DB.db");
+        }
+
+    }
+}
